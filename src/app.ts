6e175dee3d571f4e828f0893773b783b111e4777
cherryvue <yetiasg@gmail.com>
2022-01-13 12:44:10 +0100

    Prepare for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,28 @@
-import express from 'express'
-import morgan from 'morgan'
-import './dbconnection'
-import { ApolloServer } from 'apollo-server-express'
-import { graphqlSchema } from './schema'
-import { config } from './config'
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import "./dbconnection";
+import { ApolloServer } from "apollo-server-express";
+import { graphqlSchema } from "./schema";
+import { config } from "./config";
 
-const startServer = async() => {
-  const app = express()
-  app.use(morgan('dev'))
+const startServer = async () => {
+  const app = express();
+  if (config.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+  app.use(helmet());
+  app.use(cors());
 
-  const server = new ApolloServer({schema: graphqlSchema})
-  await server.start()
-  server.applyMiddleware({app, path: '/ql'})
+  const server = new ApolloServer({ schema: graphqlSchema });
+  await server.start();
+  server.applyMiddleware({ app, path: "/ql" });
   await new Promise<void>((resolve) => {
-    resolve
-    app.listen(config.server.PORT, async() => {
-      console.log(`Listening on port ${config.server.PORT}`)
-    })
-  })
-}
-startServer()
+    resolve;
+    app.listen(config.server.PORT, async () => {
+      console.log(`Listening on port ${config.server.PORT}`);
+    });
+  });
+};
+startServer();
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,13 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 export const config = {
+  NODE_ENV: process.env.NODE_ENV,
   server: {
-    PORT: process.env.PORT
-  }
-}
+    PORT: process.env.PORT || 3005,
+  },
+  db: {
+    MONGO_DB_USERNAME: process.env.MONGO_DB_USERNAME,
+    MONGO_BD_PASSWORD: process.env.MONGO_BD_PASSWORD,
+  },
+};
